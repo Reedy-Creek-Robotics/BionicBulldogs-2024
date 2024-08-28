86caf8edbe4203e1254c7f0bb2fa3a3cc4f5d7a8
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2024-08-28 07:45:51 -0400

    added test opmode and module for the external imu

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/drive/SparkfunImuLocalizer.kt
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.modules.drive
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.localization.Localizer
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+
+class SparkfunImuLocalizer(override var poseEstimate: Pose2d, override val poseVelocity: Pose2d?):
+	Localizer
+{
+	private lateinit var imu: SparkFunOTOS;
+
+	constructor(imu2: SparkFunOTOS): this(Pose2d(), Pose2d())
+	{
+		imu = imu2;
+		imu.angularUnit = AngleUnit.RADIANS;
+		imu.angularScalar = 1.0;
+		imu.calibrateImu();
+		imu.resetTracking();
+		imu.position = SparkFunOTOS.Pose2D(0.0, 0.0, 0.0);
+	}
+
+	override fun update()
+	{
+		poseEstimate = Pose2d(0.0, 0.0, imu.position.h);
+	}
+}
