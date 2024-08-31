adfc87b4014b4deb85606b2138878fd0fa822a63
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2024-08-31 11:46:13 -0400

    tweaked protobot controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/telop/ProtobotTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/telop/ProtobotTest.kt
@@ -1,13 +1,22 @@
 package org.firstinspires.ftc.teamcode.opmode.telop
 
+import com.acmerobotics.dashboard.config.Config
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 
 @TeleOp
+@Config
 class ProtobotTest: OpMode()
 {
+	companion object
+	{
+		@JvmField
+		var speed = 0.6;
+		@JvmField
+		var turnSpeed = 0.3;
+	}
 	private lateinit var leftMotor: DcMotor;
 	private lateinit var rightMotor: DcMotor;
 	override fun init()
@@ -15,13 +24,14 @@ class ProtobotTest: OpMode()
 		leftMotor = hardwareMap.dcMotor.get("left");
 		rightMotor = hardwareMap.dcMotor.get("right");
 		rightMotor.direction = DcMotorSimple.Direction.REVERSE;
+		leftMotor.direction = DcMotorSimple.Direction.REVERSE;
 	}
 
 	override fun loop()
 	{
-		val forward = gamepad1.left_stick_y;
-		val turn = gamepad1.right_stick_x;
-		leftMotor.power = (forward + turn).toDouble();
-		rightMotor.power = (forward - turn).toDouble();
+		val forward = gamepad1.left_stick_y * speed;
+		val turn = gamepad1.right_stick_x * turnSpeed;
+		leftMotor.power = (forward - turn);
+		rightMotor.power = (forward + turn);
 	}
 }
