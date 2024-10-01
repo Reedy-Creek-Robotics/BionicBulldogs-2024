f049229df5448186cbfa9950c8a13b2f4c1b51e3
Crowlex21 <110202006+Crowlex21@users.noreply.github.com>
2024-10-01 19:09:04 -0400

    FRHDriveTest.kt edited

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/telop/FRHDriveTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/telop/FRHDriveTest.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmode.telop
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.IMU
+import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.teamcode.modules.drive.IMULocalizer
 import org.firstinspires.ftc.teamcode.modules.drive.HDrive
 import org.firstinspires.ftc.teamcode.modules.hardware.ImuEx
@@ -11,22 +12,43 @@ import org.firstinspires.ftc.teamcode.opmode.config.HDriveConfig
 @TeleOp(group = "test")
 class FRHDriveTest: LinearOpMode()
 {
-		private lateinit var hDrive: HDrive;
+		private lateinit var arm: Servo
+		private lateinit var _rotate: Servo
+		private lateinit var hDrive: HDrive
+
+
 		override fun runOpMode()
 		{
-				hDrive = HDrive(HDriveConfig(hardwareMap));
+				hDrive = HDrive(HDriveConfig(hardwareMap))
 
-				val imu = ImuEx(hardwareMap.get(IMU::class.java, "imu"));
-				hDrive.setLocalizer(IMULocalizer(imu));
+				arm = hardwareMap.servo.get("arm")
+				_rotate = hardwareMap.servo.get("rotate")
+				val imu = ImuEx(hardwareMap.get(IMU::class.java, "imu"))
 
-				waitForStart();
+				hDrive.setLocalizer(IMULocalizer(imu))
+
+				waitForStart()
 				while(opModeIsActive())
 				{
-						val forward = gamepad1.left_stick_y;
-						val right = gamepad1.left_stick_x;
-						val rotate = gamepad1.right_stick_x;
-						hDrive.driveFR(forward, right, rotate);
-						hDrive.telem(telemetry);
+						val forward = gamepad1.left_stick_y
+						val right = gamepad1.left_stick_x
+						val rotate = gamepad1.right_stick_x
+						hDrive.driveFR(forward, right, rotate)
+						hDrive.telem(telemetry)
+
+						if (gamepad1.left_bumper) {
+							arm.position = 0.0
+						}
+						if (gamepad1.right_bumper) {
+							arm.position = 0.5
+						}
+						if (gamepad1.x) {
+							_rotate.position = 0.0
+						}
+						if (gamepad1.b) {
+							_rotate.position = 0.25
+						}
+
 				}
 		}
 }
