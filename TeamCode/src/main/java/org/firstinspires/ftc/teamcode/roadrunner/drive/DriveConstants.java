a859152ece2a18218776437d4b3f2e4cef85db83
xander <xand3rc0lby@gmail.com>
2024-10-01 10:01:07 -0400

    Feedforward params implemented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -58,7 +58,7 @@ public class DriveConstants {
      */
     public static double kV = 0.0121;
     public static double kA = 0.0023;
-    public static double kStatic = 0.15;
+    public static double kStatic = 0.01;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
