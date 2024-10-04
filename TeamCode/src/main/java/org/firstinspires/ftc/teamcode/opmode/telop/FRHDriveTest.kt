3dbb545d9d19d6368205bf142311e7c1510b2e7b
xander <xand3rc0lby@gmail.com>
2024-10-04 10:29:40 -0400

    Gripper problem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/telop/FRHDriveTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/telop/FRHDriveTest.kt
@@ -32,12 +32,12 @@ class FRHDriveTest: LinearOpMode()
 		hDrive = HDrive(HDriveConfig(hardwareMap))
 
 		arm = hardwareMap.servo.get("arm")
-		rotateServo = hardwareMap.crservo.get("rotate")
+		//rotateServo = hardwareMap.crservo.get("rotate")
 		claw = hardwareMap.servo.get("claw")
 		slide = hardwareMap.dcMotor.get("slide")
-		/*gripper = hardwareMap.servo.get("gripper")
+		gripper = hardwareMap.servo.get("gripper")
 		rotator0 = hardwareMap.crservo.get("rotator0")
-		rotator1 = hardwareMap.crservo.get("rotator1")*/
+		rotator1 = hardwareMap.crservo.get("rotator1")
 
 		slide.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
 		slide.direction = DcMotorSimple.Direction.REVERSE
@@ -49,6 +49,7 @@ class FRHDriveTest: LinearOpMode()
 		waitForStart()
 
 		slide.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+		slide.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
 		/*slide.targetPosition = 0
 		slide.mode = DcMotor.RunMode.RUN_TO_POSITION*/
 
@@ -61,11 +62,12 @@ class FRHDriveTest: LinearOpMode()
 			hDrive.driveFR(forward, right, rotate)
 			hDrive.telem(telemetry)
 
-			jMech()
+			cMech()
 
 			telemetry.addData("forward power:", forward)
 			telemetry.addData("strafe power:", right)
 			telemetry.addData("rotate power:", rotate)
+			telemetry.addData("arm pos: ", arm.position)
 
 			telemetry.update()
 
@@ -83,7 +85,8 @@ class FRHDriveTest: LinearOpMode()
 			arm.position = 0.63
 		}; if (gamepad1.x) {
 			rotateServo.power = -0.5
-		}; if (gamepad1.b) {
+			//changed from gamepad.b -> gamepad.y
+		}; if (gamepad1.y) {
 			rotateServo.power = 0.5
 		}; if (gamepad1.a) {
 			rotateServo.power = 0.0
@@ -99,7 +102,7 @@ class FRHDriveTest: LinearOpMode()
 		if (gamepad1.dpad_left) {
 			claw.position = 0.0
 		}; if (gamepad1.dpad_right) {
-			claw.position = 0.5
+			claw.position = 1.0
 		};
 
 		/* RUN_TO_POSITION
@@ -113,9 +116,13 @@ class FRHDriveTest: LinearOpMode()
 		}; slide.targetPosition = (slide.targetPosition).absoluteValue*/
 
 		//No target position
-		slide.power = (gamepad1.right_trigger).toDouble()
-		slide.power = -(gamepad1.left_trigger).toDouble()
-
+		if (gamepad1.right_trigger >= 0.25) {
+			slide.power = (gamepad1.right_trigger).toDouble()
+		} else if (gamepad1.left_trigger >= 0.25) {
+			slide.power = -(gamepad1.left_trigger).toDouble()
+		} else {
+			slide.power = 0.0
+		}
 
 		telemetry.addData("slide pos:", slide.currentPosition)
 		telemetry.addData("slide target:", slide.targetPosition)
@@ -127,8 +134,12 @@ class FRHDriveTest: LinearOpMode()
 
 		//Caleb's mechanism
 
-		if (gamepad1.x) {
-			gripper.position = 0.0
+		if (gamepad1.left_bumper) {
+			arm.position = 0.95
+		}; if (gamepad1.right_bumper) {
+			arm.position = 0.5
+		}; if (gamepad1.x) {
+			gripper.position = 0.01
 		}; if (gamepad1.b) {
 			gripper.position = 0.25
 		}; if (gamepad1.dpad_up) {
@@ -141,5 +152,8 @@ class FRHDriveTest: LinearOpMode()
 			rotator0.power = 0.0
 			rotator1.power = 0.0
 		}
+
+		telemetry.addData("grip pos: ", gripper.position)
+
 	}
 }
