942302fd993db6c56bf38caa4a7c92399b772ed5
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2025-06-01 12:35:41 -0400

    added windows build of pack and test opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lua/lua/testOpmode.lua
@@ -0,0 +1,18 @@
+local action;
+
+addOpmode({
+	name = "testOpmode",
+	init = function ()
+		action = sequentalAction()
+				:add(sleepAction(4))
+				:add(markerAction("m1"))
+				:add(sleepAction(6))
+				:add(markerAction("m2"))
+				:add(sleepAction(8))
+				:add(markerAction("m3"))
+				:build();
+	end,
+	start = function ()
+		runTimer(action, "testOpmode.txt");
+	end
+})
